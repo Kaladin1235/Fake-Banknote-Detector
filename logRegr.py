5e9014613cd7ae005b8d2ebd57c85c489871ea26
Kaladin1235 <ialdanondo@yahoo.es>
2023-08-28 10:35:29 +0200

    Update logRegr.py

--- logRegr.py
+++ logRegr.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 df = pd.DataFrame(pd.read_csv("data_banknote_authentication.csv"))
 
